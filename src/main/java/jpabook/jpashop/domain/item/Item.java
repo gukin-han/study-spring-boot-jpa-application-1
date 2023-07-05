f074e56dafc36934ac7a132bb08c3939c87491c1
Gukin Han <gukin.dev@gmail.com>
2023-07-05 11:36:32 +0900

    섹션2: 엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -24,4 +24,6 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
