c1e6618e3262a04c371fb62e693c82dbbe1e3275
Gukin Han <gukin.dev@gmail.com>
2024-01-15 19:08:01 +0900

    회원 리파지토리 개발 end

---
+++ src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // 어노테이션이 있으면 스프링이 주입을 해준다
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList(); // entity 오브젝트에 대한 조회를 한다 (차이점)
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
